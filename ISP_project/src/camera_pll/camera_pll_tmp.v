82b98d231e4f7931df1ee8236f2e22d8da4f31a4
love_saber <love_saber1983@outlook.com>
2024-09-27 21:51:14 +0800

    maked initial project

---
+++ ISP_project/src/camera_pll/camera_pll_tmp.v
@@ -0,0 +1,19 @@
+//Copyright (C)2014-2023 Gowin Semiconductor Corporation.
+//All rights reserved.
+//File Title: Template file for instantiation
+//GOWIN Version: V1.9.9 Beta-5
+//Part Number: GW5A-LV25UG324ES
+//Device: GW5A-25
+//Device Version: A
+//Created Time: Thu Oct 12 11:00:33 2023
+
+//Change the instance name and port connections to the signal names
+//--------Copy here to design--------
+
+    camera_pll your_instance_name(
+        .clkout0(clkout0_o), //output clkout0
+        .clkout1(clkout1_o), //output clkout1
+        .clkin(clkin_i) //input clkin
+    );
+
+//--------Copy end-------------------
